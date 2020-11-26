16c785637aa6078c7a2dfde4637eab0e20b86f9d
wanyi007 <wanyi@moyideMacBook-Pro.local>
2020-11-26 11:06:20 +0800

    feat:判断平衡二叉树

---
+++ src/main/java/com/huobi/leetcode/simple/平衡二叉树.java
@@ -0,0 +1,35 @@
+package com.huobi.leetcode.simple;
+
+import com.huobi.leetcode.utils.TreeNode;
+
+/**
+ * @author wanyi
+ * @version V1.0
+ *
+ * 给定一个二叉树 判断它是否是高度平衡二叉树
+ *
+ * 本题中，一棵高度平衡二叉树定义为：
+ *
+ * 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1 。
+ *
+ *
+ *
+ * @Package com.huobi.leetcode.simple
+ * @date 2020-11-26 10:47
+ */
+public class 平衡二叉树 {
+
+    public boolean isBalanced(TreeNode root){
+        if (root == null){
+            return true;
+        }
+        return Math.abs(depth(root.left)-depth(root.right))<=1 && isBalanced(root.right) && isBalanced(root.right);
+    }
+
+    public int depth(TreeNode root){
+        if (root == null ){
+            return 0;
+        }
+        return Math.max(depth(root.left),depth(root.right))+1;
+    }
+}
